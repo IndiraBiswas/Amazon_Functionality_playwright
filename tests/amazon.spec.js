08f57ef48f80dcae5eeb3cd0b6d6a046cb4fa621
Indira Biswas <95736540+IndiraBiswas@users.noreply.github.com>
2024-05-09 22:13:34 +0530

    amazon.spec.js

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -6,13 +6,13 @@ test('test', async ({ page }) => {
     await page.goto('https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fs%2F%3F_encoding%3DUTF8%26adgrpid%3D138506081990%26ext_vrnc%3Dhi%26gclid%3DEAIaIQobChMI-f6Op_nzhQMVeFoPAh10fQVoEAAYASAAEgJjmvD_BwE%26hvadid%3D674474151981%26hvdev%3Dc%26hvdvcmdl%3D%26hvlocint%3D%26hvlocphy%3D9040229%26hvnetw%3Dg%26hvpone%3D%26hvpos%3D%26hvptwo%3D%26hvqmt%3De%26hvrand%3D4502655422014069201%26hvtargid%3Dkwd-24787317188%26hydadcr%3D564_2683761%26ie%3DUTF8%26index%3Daps%26keywords%3Damazon%2520sign%2520in%26ref%3Dpd_sl_39ebeum5qu_e%26tag%3Dgooginhydr1-21%26ref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
    
     // Enter Amazon Username 
-    await page.getByLabel('Email or mobile phone number').fill('biswasindira15@gmail.com');
+    await page.getByLabel('Email or mobile phone number').fill("<your amazon.in email/phone number>");
     await page.getByLabel('Continue').click();
    
     // Enter Amazon password
     
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('Indira2727#');
+    await page.getByLabel('Password').fill("<your amazon.in password>");
     await page.getByLabel('Sign in').click();
     await page.getByRole('button', { name: 'Go' }).click();
     console.log("Login Successfull")
